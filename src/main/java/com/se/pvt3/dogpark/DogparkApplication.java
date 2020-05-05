5ab2306393a03102808de1f74e297330508bd340
haj300 <katja.lindeberg@gmail.com>
2020-05-06 00:14:27 +0200

    intialize

---
+++ src/main/java/com/se/pvt3/dogpark/DogparkApplication.java
@@ -0,0 +1,13 @@
+package com.se.pvt3.dogpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogparkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/se/pvt3/dogpark/DogparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se.pvt3.dogpark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
